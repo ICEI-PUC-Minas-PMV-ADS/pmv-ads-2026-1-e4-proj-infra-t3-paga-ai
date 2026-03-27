2c60b0b2f93b4725835e0b57178c4eee58376aa0
FlavioMoreira1612 <flaviol.moreira@icloud.com>
2026-03-27 20:42:54 -0300

    clients

--- obj/Debug/net9.0/Clientes.AssemblyInfo.cs
+++ obj/Debug/net9.0/Clientes.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Clientes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af0129ef803da1fadc662b6a72881bc5de643c01")]
 [assembly: System.Reflection.AssemblyProductAttribute("Clientes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Clientes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
