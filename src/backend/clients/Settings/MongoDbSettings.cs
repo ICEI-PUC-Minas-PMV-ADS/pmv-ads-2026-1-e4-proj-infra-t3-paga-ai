869daf952f3a616813082eb621ff48a4e2bced34
FlavioMoreira1612 <flaviol.moreira@icloud.com>
2026-04-02 22:35:36 -0300

    ajustante o MongoDB Settings para elliminar warnings de nao anulaveis

--- src/backend/clients/Settings/MongoDbSettings.cs
+++ src/backend/clients/Settings/MongoDbSettings.cs
@@ -2,8 +2,8 @@ namespace ApiClientes.Settings
 {
     public class MongoDbSettings
     {
-        public string ConnectionString { get; set; }
-        public string DatabaseName { get; set; }
-        public string ClientesCollectionName { get; set; }
+        public string? ConnectionString { get; set; }
+        public string? DatabaseName { get; set; }
+        public string? ClientesCollectionName { get; set; }
     }
 }
