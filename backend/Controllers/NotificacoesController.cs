6a3a94702be1a93062c1e6dec164149fec03fa18
alex <alex.junio.1482544@sga.pucminas.br>
2026-04-03 14:37:51 -0300

    feat: notificacoes

---
+++ backend/Controllers/NotificacoesController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+using backend.Models;
+
+[ApiController]
+[Route("api/[controller]")]
+
+public class NotificacoesController : ControllerBase
+{
+    private readonly IMongoCollection<Notificacao> _notificacoes;
+
+    public NotificacoesController(IMongoDatabase database)
+    {
+        _notificacoes = database.GetCollection<Notificacao>("notificacoes");
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Notificacao>> Get(string id)
+    {
+        var notificacao = await _notificacoes.Find(x => x.Id == id).FirstOrDefaultAsync();
+        if (notificacao == null)
+        {
+            return NotFound();
+        }
+
+        return notificacao;
+
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Post(Notificacao novo)
+    {
+        await _notificacoes.InsertOneAsync(novo);
+        return Ok(novo);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Update(string id, Notificacao notificacaoLida)
+    {
+        var notificacao = await _notificacoes.Find(x => x.Id == id).FirstOrDefaultAsync();
+
+        if (notificacao == null) return NotFound();
+
+        notificacaoLida.Id = notificacao.Id;
+
+        await _notificacoes.ReplaceOneAsync(x => x.Id == id, notificacaoLida);
+
+        return NoContent();
+    }
+}
---
+++ backend/Models/Notificacoes.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace backend.Models;
+
+
+public class Notificacao
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string? Id { get; set; }
+
+    public string? Mensagem { get; set; }
+
+    public bool Lida { get; set; } = false;
+
+    public DateTime DataCriacao { get; set; } = DateTime.UtcNow;
+
+    public int? EmprestimoId { get; set; }
+}
+
