e90e46af22300ed7d246ce947cfb3fd694a342ce
FlavioMoreira1612 <flaviol.moreira@icloud.com>
2026-04-02 22:13:42 -0300

    ajustes para IOptions MongoDB

--- src/backend/clients/Models/Cliente.cs
+++ src/backend/clients/Models/Cliente.cs
@@ -7,13 +7,13 @@ namespace ApiClientes.Models
     {
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
-        public string Id { get; set; }
+        public string? Id { get; set; }
 
-        public string Nome { get; set; }
-        public string CPF { get; set; }
-        public string Telefone { get; set; }
-        public string Endereco { get; set; }
-        public string Email { get; set; }
-        public string Descricao { get; set; }
+        public string? Nome { get; set; }
+        public string? CPF { get; set; }
+        public string? Telefone { get; set; }
+        public string? Endereco { get; set; }
+        public string? Email { get; set; }
+        public string? Descricao { get; set; }
     }
 }
